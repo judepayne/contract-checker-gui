ad9f142583607af5c0ad8a21e8c82abfdd6f8591
Gunjan <gunjan \.kothari4@gmail.com>
2020-01-23 09:18:03 +0000

    adding .gitignore

--- public/js/cljs-runtime/goog.history.eventtype.js
+++
@@ -1,4 +0,0 @@
-goog.provide("goog.history.EventType");
-/** @enum {string} */ goog.history.EventType = {NAVIGATE:"navigate"};
-
-//# sourceMappingURL=goog.history.eventtype.js.map
--- public/js/cljs-runtime/org.bluebird.js
+++
@@ -1,6 +0,0 @@
-goog.provide('org.bluebird');
-goog.require('cljs.core');
-var module$node_modules$bluebird$js$browser$bluebird=shadow.js.require("module$node_modules$bluebird$js$browser$bluebird", {});
-goog.exportSymbol("Promise",module$node_modules$bluebird$js$browser$bluebird);
-
-//# sourceMappingURL=org.bluebird.js.map
