57fa231d62d6cbb4eae2525413a20528efacb31a
Jude Payne <judepayne@yahoo.co.uk>
2020-01-31 13:07:34 +0000

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 shadow-cljs release app
 
-aws s3 sync public s3://draw-graph --acl "public-read"
-
-aws s3 sync public s3://draw-graph-us --acl "public-read"
-
-aws s3 sync public s3://draw-graph.com --acl "public-read"
+aws s3 sync public s3://contract-checker --acl "public-read"
